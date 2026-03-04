2a929ffd2fd9aa463c17cceb6d74b83bc3d10000
Chris Williams <chris@chrispwill.com>
2026-03-04 15:36:48 +0900

    Final naming tweaks

--- cli/tests/nushell_cmd.rs
+++ cli/tests/nushell_cmd.rs
@@ -3,7 +3,7 @@ use std::{fs, process::Output};
 use assert_cmd::Command;
 
 pub mod common;
-use common::{TestVals, build_test_vals};
+use common::{Fixtures, build_fixtures};
 
 const NUSHELL_ENV_CHECK: &str = r#"
 let json_data = '{{.JsonBlob}}' | from json
@@ -17,23 +17,25 @@ if ($invalid | length) > 0 {
 }
 "#;
 
-fn load_nu_env_string(test_vals: &TestVals, current_dir: &str) -> String {
+fn load_nu_env_string(test_vals: &Fixtures, current_dir: &str) -> String {
     format!(
-            "envoluntary shell export nushell --config-path {config_path} --cache-dir {cache_dir} --current-dir {current_dir} | from json --objects | default {{}} | reduce --fold {{}} {{|row, acc| $acc | merge $row}}
- | load-env",
-            config_path = &test_vals.config_file.to_string_lossy(),
-            cache_dir = &test_vals.cache_dir.path().to_string_lossy(),
-            current_dir = current_dir
-        )
+        "envoluntary shell export nushell --config-path {config_path} --cache-dir {cache_dir} --current-dir {current_dir} | from json --objects | default {{}} | reduce --fold {{}} {{|row, acc| $acc | merge $row}} | load-env",
+        config_path = &test_vals.config_file.to_string_lossy(),
+        cache_dir = &test_vals.cache_dir.path().to_string_lossy(),
+        current_dir = current_dir
+    )
 }
+
+// TODO: Ideally Inputs will be extracted to the common module, and other shells could use a similar API for checking behaviour
 
 #[derive(Default)]
 struct Inputs<'a> {
     current_dir: String,
     expected_env: serde_json::Value,
-    test_vals: Option<&'a TestVals>,
-    home_dir: Option<String>,
-    extra_script: Option<String>,
+    /// Set this to first "cd" to this directory before going to `current_dir`
+    previous_dir: Option<String>,
+    fixtures: Option<&'a Fixtures>,
+    home_dir_override: Option<String>,
 }
 
 impl<'a> Inputs<'a> {
@@ -50,16 +52,16 @@ fn export_and_check(inputs: &Inputs) -> Output {
     let Inputs {
         expected_env,
         current_dir,
-        test_vals: maybe_test_vals,
-        home_dir,
-        extra_script,
+        fixtures: maybe_fixtures,
+        home_dir_override: home_dir,
+        previous_dir,
     } = inputs;
-    let default_test_vals = build_test_vals();
-    let test_vals = maybe_test_vals.unwrap_or(&default_test_vals);
+    let default_test_vals = build_fixtures();
+    let fixtures = maybe_fixtures.unwrap_or(&default_test_vals);
     let mut cmd = Command::new("nu");
     let env_check_script = NUSHELL_ENV_CHECK.replace("{{.JsonBlob}}", &expected_env.to_string());
-    let script = if let Some(ext_script) = extra_script {
-        ext_script.clone().to_owned() + ";\n" + &env_check_script
+    let script = if let Some(dir) = previous_dir {
+        load_nu_env_string(fixtures, dir) + ";\n" + &env_check_script
     } else {
         env_check_script
     };
@@ -67,11 +69,11 @@ fn export_and_check(inputs: &Inputs) -> Output {
         "-c",
         &format!(
             "{load_env};\n{script}",
-            load_env = load_nu_env_string(test_vals, current_dir),
+            load_env = load_nu_env_string(fixtures, current_dir),
             script = script
         ),
     ])
-    .env("PATH", &test_vals.path)
+    .env("PATH", &fixtures.path)
     .env(
         "HOME",
         home_dir.clone().unwrap_or_else(|| "/home".to_string()),
@@ -113,30 +115,35 @@ fn test_update_existing_state() {
         "ENVOLUNTARY_ENV_STATE": "KLUv/QQ4HQQAHAcAeyJmbGFrZV9yZWZlcmVuY2VzIjpbImdpdGh1YjpvdGhlcl9fb3duZXIvcmVwbyJdLCJlbnZfdmFyc19yZXNldCI6eyJGQUtFX1ZBUiI6bnVsbCwiRU5WT0xVTlRBUllfRU5WX1NUQVRFIjpudWxsfX0BqBDj//0Qww8Qow+DMUZWbCwq",
     });
 
-    let test_vals = build_test_vals();
+    let test_vals = build_fixtures();
     export_and_check(&Inputs {
-        extra_script: Some(load_nu_env_string(&test_vals, "/home/some/other/dir")),
-        test_vals: Some(&test_vals),
+        previous_dir: Some("/home/some/other/dir".to_string()),
+        fixtures: Some(&test_vals),
         ..Inputs::new("/some/dir", expected_update_json)
     });
 }
 
 #[test]
 fn test_export_twice_in_same_dir() {
-    let test_vals = build_test_vals();
+    let expected_update_json = serde_json::json!({
+        "FAKE_VAR": "true",
+        "ENVOLUNTARY_ENV_STATE": "KLUv/QQ4HQQAHAcAeyJmbGFrZV9yZWZlcmVuY2VzIjpbImdpdGh1YjpvdGhlcl9fb3duZXIvcmVwbyJdLCJlbnZfdmFyc19yZXNldCI6eyJGQUtFX1ZBUiI6bnVsbCwiRU5WT0xVTlRBUllfRU5WX1NUQVRFIjpudWxsfX0BqBDj//0Qww8Qow+DMUZWbCwq",
+    });
+
+    let test_vals = build_fixtures();
     export_and_check(&Inputs {
-        extra_script: Some(load_nu_env_string(&test_vals, "/home/some/other/dir")),
-        test_vals: Some(&test_vals),
-        ..Inputs::new("/home/some/other/dir", serde_json::json!({}))
+        previous_dir: Some("/home/some/other/dir".to_string()),
+        fixtures: Some(&test_vals),
+        ..Inputs::new("/home/some/other/dir", expected_update_json)
     });
 }
 
 #[test]
 fn test_reset_state() {
-    let test_vals = build_test_vals();
+    let test_vals = build_fixtures();
     export_and_check(&Inputs {
-        extra_script: Some(load_nu_env_string(&test_vals, "/")),
-        test_vals: Some(&test_vals),
+        previous_dir: Some("/".to_string()),
+        fixtures: Some(&test_vals),
         ..Inputs::new(
             "/home/some/other/dir",
             serde_json::json!({"ENVOLUNTARY_ENV_STATE": null}),
@@ -146,16 +153,16 @@ fn test_reset_state() {
 
 #[test]
 fn test_adjacent_pattern_matching() {
-    let adjacent_test_vals = build_test_vals();
+    let adjacent_test_vals = build_fixtures();
     let bin_dir = &adjacent_test_vals.bin_dir.to_string_lossy().into_owned();
     export_and_check(&Inputs {
-        test_vals: Some(&adjacent_test_vals),
+        fixtures: Some(&adjacent_test_vals),
         ..Inputs::new(bin_dir, serde_json::json!({"ENVOLUNTARY_ENV_STATE": null}))
     });
 
     fs::File::create_new(adjacent_test_vals.work_dir.path().join(".supercooltool")).unwrap();
     export_and_check(&Inputs {
-        test_vals: Some(&adjacent_test_vals),
+        fixtures: Some(&adjacent_test_vals),
         ..Inputs::new(
             bin_dir,
             serde_json::json!({
@@ -173,7 +180,7 @@ fn test_adjacent_home_pattern_matching() {
     fs::File::create_new(home_dir.path().join(".awesometool")).unwrap();
 
     export_and_check(&Inputs {
-        home_dir: Some(home_path.to_string()),
+        home_dir_override: Some(home_path.to_string()),
         ..Inputs::new(
             &home_path,
             serde_json::json!({
